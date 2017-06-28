6225de8c13bdbc38c96276babf86038026a2b8fa
shashikantdurge <annabday@46>
2017-06-28 11:46:29 +0530

    Syllabus fragment-plus button and the expandable button are added

--- app/src/main/java/com/projects/psps/bmsce/MainActivity.java
+++ app/src/main/java/com/projects/psps/bmsce/MainActivity.java
@@ -1,13 +1,37 @@
 package com.projects.psps.bmsce;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_main_chat:
+                //change the icon
+                Toast.makeText(MainActivity.this, "CHAT", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_main_event:
+                Toast.makeText(MainActivity.this, "EVENT", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_main_syllabus:
+                Toast.makeText(MainActivity.this, "SYLLABUS", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
